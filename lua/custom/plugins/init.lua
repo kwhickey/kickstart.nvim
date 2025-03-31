7bd881c4dfcab81cac1407a7f0e8cc0f1f38064a
Keith Hickey <hickey_keith@bah.com>
2025-03-31 10:02:20 -0400

    Neovim config as of 2025-03-29 v3 -- using built-in neotree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,32 +3,32 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    init = function()
-      -- Below funciton allows opening a directory with an empty buffer using: `nvim <directory>`
-      if vim.fn.argc(-1) == 1 then
-        local stat = vim.loop.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == 'directory' then
-          require('neo-tree').setup {
-            filesystem = {
-              hijack_netrw_behavior = 'open_current',
-            },
-          }
-        end
-      end
-    end,
-    opts = {
-      -- neo-tree options here
-    },
-  },
+  --{
+  --'nvim-neo-tree/neo-tree.nvim',
+  --branch = 'v3.x',
+  --dependencies = {
+  --'nvim-lua/plenary.nvim',
+  --'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  --'MunifTanjim/nui.nvim',
+  ---- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  --},
+  --init = function()
+  ---- Below funciton allows opening a directory with an empty buffer using: `nvim <directory>`
+  --if vim.fn.argc(-1) == 1 then
+  --local stat = vim.loop.fs_stat(vim.fn.argv(0))
+  --if stat and stat.type == 'directory' then
+  --require('neo-tree').setup {
+  --filesystem = {
+  --hijack_netrw_behavior = 'open_current',
+  --},
+  --}
+  --end
+  --end
+  --end,
+  --opts = {
+  ---- neo-tree options here
+  --},
+  --},
   {
     -- lsp_lines, used to provide multi-line and character-specific error/diagonstic info
     'ErichDonGubler/lsp_lines.nvim',
